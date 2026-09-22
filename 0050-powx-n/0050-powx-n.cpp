692478c5eecd0bf1a9a2b34f2a91942fb158fcdb
Diivyaa2163 <divyakukade2163@gmail.com>
2026-09-22 18:32:02 +0530

    Time: 0 ms (100%), Space: 8.5 MB (93.11%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,26 +3,26 @@ public:
     double myPow(double x, int n) {
 
         
-        // // EDGE CASES
-        // if (n == 0) {
-        //     return 1.0;
-        // }
+        // EDGE CASES
+        if (n == 0) {
+            return 1.0;
+        }
 
-        // if (x == 0) {
-        //     return 0.0;
-        // }
+        if (x == 0) {
+            return 0.0;
+        }
 
-        // if (x == 1) {
-        //     return 1.0;
-        // }
+        if (x == 1) {
+            return 1.0;
+        }
 
-        // if ((x == -1) &&  (n%2 == 0)) {
-        //     return 1.0;
-        // }
+        if ((x == -1) &&  (n%2 == 0)) {
+            return 1.0;
+        }
 
-        // if ((x == -1) &&  (n%2 != 0)) {
-        //     return -1.0;
-        // }
+        if ((x == -1) &&  (n%2 != 0)) {
+            return -1.0;
+        }
 
 
         long binForm = n;
