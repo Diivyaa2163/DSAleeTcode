e6558aa1dccf0842b54a46bb202a3d29d4c74536
Diivyaa2163 <divyakukade2163@gmail.com>
2026-09-22 21:52:42 +0530

    Time: 0 ms (100%), Space: 40.1 MB (95.5%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -2,7 +2,56 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
 
-        // M2: BRUTE FORCE APPROACH (O(n^2))
+        // M1:  Prefix * Suffix [ Time Complexity = O(3n) == O(n); Space Complexity = O(1) ]
+        int n = nums.size();
+        vector<int> ans(n, 1);
+
+        // Storing variable value of prefix & suffix within the ans itself
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+
+        // Time Complexity: O(n); Space Complexity: O(1)
+        int suffix = 1;
+        for (int i = (n - 2); i >= 0; i--) {
+            suffix *= nums[i+1];    // ith suffix
+            ans[i] *= suffix;
+        }
+
+        return ans;
+
+
+
+
+
+
+
+    // //M2: Prefix * Suffix [ Time Complexity = O(3n) == O(n); Space Complexity = O(3n) == O(n) ]
+    // int n = nums.size();
+    // vector<int> ans(n, 1);
+
+    // // Time Complexity: O(n); Space Complexity: O(n)
+    // vector<int> prefix(n, 1);
+    //     prefix[0] = 1;
+    // for (int i = 1; i < n; i++) {
+    //     prefix[i] = prefix[i-1] * nums[i-1];
+    // }
+
+    // // Time Complexity: O(n); Space Complexity: O(n)
+    // vector<int> suffix(n, 1);
+    //     suffix[n-1] = 1;
+    // for (int i = (n - 2); i >= 0; i--) {
+    //     suffix[i] = suffix[i+1] * nums[i+1];
+    // }
+
+    // // Time Complexity: O(n); Space Complexity: O(n)
+    // for (int i = 0; i < n; i++) {
+    //     ans[i] = prefix[i] * suffix[i];
+    // }
+    // return ans;
+
+
+    //    // M2: BRUTE FORCE APPROACH (O(n^2))
     //     int n = nums.size();
 
     //     vector<int> ans(n, 1);
@@ -15,31 +64,6 @@ public:
     //     }
     // }
     // return ans;
-
-
-    //M3: Prefix * Suffix [ Time Complexity = O(3n) == O(n); Space Complexity = O(3n) == O(n) ]
-    int n = nums.size();
-    vector<int> ans(n, 1);
-
-    // Time Complexity: O(n); Space Complexity: O(n)
-    vector<int> prefix(n, 1);
-        prefix[0] = 1;
-    for (int i = 1; i < n; i++) {
-        prefix[i] = prefix[i-1] * nums[i-1];
-    }
-
-    // Time Complexity: O(n); Space Complexity: O(n)
-    vector<int> suffix(n, 1);
-        suffix[n-1] = 1;
-    for (int i = (n - 2); i >= 0; i--) {
-        suffix[i] = suffix[i+1] * nums[i+1];
-    }
-
-    // Time Complexity: O(n); Space Complexity: O(n)
-    for (int i = 0; i < n; i++) {
-        ans[i] = prefix[i] * suffix[i];
-    }
-    return ans;
     }
    
 };
