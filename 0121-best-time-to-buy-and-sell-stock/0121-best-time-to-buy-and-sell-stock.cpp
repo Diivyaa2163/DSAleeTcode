397f736a4c8cd471984e8c115099485b8c64f823
Diivyaa2163 <divyakukade2163@gmail.com>
2026-09-22 19:07:12 +0530

    Time: 3 ms (27.49%), Space: 97.3 MB (88.17%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,9 +4,8 @@ public:
 
         int maxProf = 0;
         int bestBuy = prices[0];
-        int n = prices.size();
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < prices.size(); i++) {
 
             if (prices[i] > bestBuy) {
                 maxProf = max(maxProf, prices[i] - bestBuy);
