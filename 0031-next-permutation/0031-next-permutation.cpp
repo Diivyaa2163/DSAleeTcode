38420494f517016f0fc45ed183355beadaac7fb5
Diivyaa2163 <divyakukade2163@gmail.com>
2026-09-25 22:39:50 +0530

    Time: 0 ms (100%), Space: 15.8 MB (54.03%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,37 +2,33 @@ class Solution {
 public:
     void nextPermutation(vector<int>& A) {
         
-        next_permutation(A.begin(), A.end());
-        // // TC: O(n), SC: O(1)
-        // int n = A.size();
-        // int pivot = -1;
-        // for (int i = n-2; i>=0; i--) {
-        //     if(A[i] < A[i+1]) {
-        //         pivot = i;
-        //         break;
-        //     }
-        // }
+        // next_permutation(A.begin(), A.end());
+        // TC: O(n), SC: O(1)
+        int n = A.size();
+        int pivot = -1;
+        for (int i = n-2; i>=0; i--) {
+            if(A[i] < A[i+1]) {
+                pivot = i;
+                break;
+            }
+        }
 
-        // // EDGE CASE FOR DESCENDING ORDER ARRAY
-        // if (pivot == -1) {
-        //     reverse(A.begin(), A.end());
-        //     return;
-        // }
+        // EDGE CASE FOR DESCENDING ORDER ARRAY
+        if (pivot == -1) {
+            reverse(A.begin(), A.end());
+            return;
+        }
 
-        // // 2nd STEP: NEXT LARGER ELEMENT
-        // for (int i = n-1; i>pivot; i--) {
-        //     if (A[i] > A[pivot]) {
-        //         swap(A[i], A[pivot]);
-        //         break;
-        //     }
-        // }
+        // 2nd STEP: NEXT LARGER ELEMENT
+        for (int i = n-1; i>pivot; i--) {
+            if (A[i] > A[pivot]) {
+                swap(A[i], A[pivot]);
+                break;
+            }
+        }
 
-        // // 3rd step: REVERSE (pivot+1 to n-1)
-        // reverse(A.begin() + pivot + 1, A.end());
+        // 3rd step: REVERSE (pivot+1 to n-1)
+        reverse(A.begin() + pivot + 1, A.end());
 
-        // // int i = pivot+1, j = n-1;
-        // // while(i <= j) {
-        // //     swap(A[i++], A[j--]);
-        // // }
     }
 };
