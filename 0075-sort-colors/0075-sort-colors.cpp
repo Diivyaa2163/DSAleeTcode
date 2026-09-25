d07727bcc4aa545f42d2b5a8e8209887f6abd6c9
Diivyaa2163 <divyakukade2163@gmail.com>
2026-09-25 21:19:46 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.26%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,28 +1,51 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+
+        // M2: DUTCH NATIONAL FLAG ALGORITHM
         int n = nums.size();
-        int count0 = 0, count1 = 0, count2 = 0;
+        int low = 0, mid = 0, high = n -1;
 
-        for (int i = 0; i<n; i++) {
-            if(nums[i] == 0) {
-                count0++;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                mid++;
+                low++;
             }
-            else if(nums[i] == 1) {
-                count1++;
+
+            else if (nums[mid] == 1) {
+                mid++;
             }
-            else count2++;
+
+            else {
+                swap(nums[high], nums[mid]);
+                high--;
+            }
         }
 
-        int id = 0;
-        for (int i = 0; i<count0; i++) {
-            nums[id++] = 0;
-        }
-        for (int i = 0; i<count1; i++) {
-            nums[id++] = 1;
-        }
-        for (int i = 0; i<count2; i++) {
-            nums[id++] = 2;
-        }
+        // M3: 2 Passes of O(n)
+        // int n = nums.size();
+        // int count0 = 0, count1 = 0, count2 = 0;
+
+        // for (int i = 0; i<n; i++) {
+        //     if(nums[i] == 0) {
+        //         count0++;
+        //     }
+        //     else if(nums[i] == 1) {
+        //         count1++;
+        //     }
+        //     else count2++;
+        // }
+
+        // int id = 0;
+        // for (int i = 0; i<count0; i++) {
+        //     nums[id++] = 0;
+        // }
+        // for (int i = 0; i<count1; i++) {
+        //     nums[id++] = 1;
+        // }
+        // for (int i = 0; i<count2; i++) {
+        //     nums[id++] = 2;
+        // }
     }
 };
